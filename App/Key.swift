dda01aecbe196bde1f6d5755d2409c4a92011bf0
Till Brügmann <till.bruegmann@icloud.com>
2026-03-18 00:19:32 +0100

    adding vapor project

---
+++ App/Key.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let KEY: String = {
+#if DEBUG
+    ""
+#else
+    "e5a2a04451db3bf277b8093eac8ee05d"
+#endif
+}()
---
+++ App/ViewModels/NetworkingManager.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+@Observable final class NetworkingManager {
+    private let url = "https://www.stoobit.com"
+    
+    private let key: String = "balance.identifier"
+    private let id: String
+    
+    init() {
+        if let id = UserDefaults.standard.string(forKey: key) {
+            self.id = id
+            return
+        }
+        
+        self.id = UUID().uuidString
+        UserDefaults.standard.set(self.id, forKey: key)
+    }
+    
+    func upload(_ check: BalanceCheckModel) async throws {
+        guard let url = URL(string: self.url) else {
+            throw NetworkingError.urlFailed
+        }
+        
+        var request = URLRequest(url: url)
+        request.setValue("authentication", forHTTPHeaderField: KEY)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let payload = NetworkingPayload(id: id, measurements: check.measurements)
+        request.httpBody = try JSONEncoder().encode(payload)
+        
+        let (_, response) = try await URLSession.shared.data(for: request)
+        
+        guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+            throw NetworkingError.invalidStatusCode(statusCode: -1)
+        }
+        guard (200...299).contains(statusCode) else {
+            throw NetworkingError.invalidStatusCode(statusCode: statusCode)
+        }
+    }
+    
+    private struct NetworkingPayload: Codable {
+        var id: String
+        var measurements: [BalanceMeasurement]
+    }
+    
+    private enum NetworkingError: Error {
+        case urlFailed
+        case invalidStatusCode(statusCode: Int)
+    }
+}
